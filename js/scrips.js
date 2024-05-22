167a80c961b475106bc016e2b3fe0d349a4bf2ab
AntonyLD <antonyldcamargo@gmail.com>
2024-05-22 10:02:21 -0300

    todo edit form create

--- js/scrips.js
+++ js/scrips.js
@@ -6,6 +6,8 @@ const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
+let oldInputValue;
+
 //Funções
 const saveTodo = (text) =>{
 
@@ -37,7 +39,25 @@ const saveTodo = (text) =>{
     todoInput.focus();
 }
 
+const toggleForms = () =>{
+    editForm.classList.toggle("hide");
+    todoForm.classList.toggle("hide")
+    todoList.classList.toggle("hide")
+}
 
+const updateTodo = (text) =>{
+
+    const todos = document.querySelectorAll(".todo")
+
+    todos.forEach((todo) =>{
+        
+        let todoTitle = todo.querySelector("h3")
+
+        if(todoTitle.innerText === oldInputValue){
+            todoTitle.innerText = text
+        }
+    })
+}
 //Eventos
 
 todoForm.addEventListener("submit", (e) =>{
@@ -49,3 +69,44 @@ todoForm.addEventListener("submit", (e) =>{
         saveTodo(inputValue)
     }
 });
+
+document.addEventListener("click", (e) =>{
+    const targetEl = e.target;
+    const parentEl = targetEl.closest("div");
+    let todoTitle;
+
+    if(parentEl && parentEl.querySelector("h3")){
+        todoTitle = parentEl.querySelector("h3").innerText;
+    }
+
+    if (targetEl.classList.contains("finish-todo")){
+        parentEl.classList.toggle("done");
+    }
+
+    if(targetEl.classList.contains("remove-todo")){
+        parentEl.remove();
+    }
+
+    if(targetEl.classList.contains("edit-todo")){
+        toggleForms()
+        
+        editInput.value = todoTitle
+        oldInputValue = todoTitle
+    }
+});
+
+cancelEditBtn.addEventListener("click", (e) =>{
+    e.preventDefault();
+
+    toggleForms();
+})
+
+editForm.addEventListener("submit", (e) =>{
+    const editInputValue = editInput.value
+
+    if(editInputValue) {
+        updateTodo(editInputValue)
+    }
+
+    toggleForms()
+})
